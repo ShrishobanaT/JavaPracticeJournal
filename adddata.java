4930d11e063db1fc5c8796a9a7966582c9e82d56
ShrishobanaT <shrishobana_skystudent@outlook.com>
2022-03-20 01:45:45 +0100

    Load my first file into the git

---
+++ adddata.java
@@ -0,0 +1,17 @@
+public class adddata
+{
+public static void main(String args[])
+{
+int tamil=100;
+int English=100;
+      int Maths=100;
+System.out.println("welcome");
+System.out.println(tamil+English+Maths);
+}
+}
+
+
+
+
+
+
