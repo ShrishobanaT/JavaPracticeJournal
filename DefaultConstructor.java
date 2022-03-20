9eb44c8bf8afeca6c66661b41e5c208d726b4cc0
ShrishobanaT <shrishobana_skystudent@outlook.com>
2022-03-20 02:27:09 +0100

    Update

---
+++ DefaultConstructor.java
@@ -0,0 +1,11 @@
+class DefaultConstructor
+{
+int a;
+float b;
+public static void main(String args[]){
+DefaultConstructor DC=new DefaultConstructor();
+
+System.out.println(DC.a);
+System.out.println(DC.b);
+}
+}
