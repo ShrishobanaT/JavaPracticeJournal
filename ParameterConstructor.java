f50a655c69cd70ebda94145350a2f78be0b4919d
ShrishobanaT <shrishobana_skystudent@outlook.com>
2022-03-20 02:28:19 +0100

    Updated

---
+++ ParameterConstructor.java
@@ -0,0 +1,12 @@
+class ParameterConstructor
+{
+ParameterConstructor(int i)
+{
+System.out.println(i);
+}
+public static void main(String args[])
+
+{
+ ParameterConstructor PC=new ParameterConstructor(5);
+}
+}
