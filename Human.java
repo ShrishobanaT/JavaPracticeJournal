b331a0627d425213f3d2c4ba7bae83ec1bfcd828
ShrishobanaT <shrishobana_skystudent@outlook.com>
2022-03-23 22:33:15 +0100

    master

---
+++ Human.java
@@ -0,0 +1,15 @@
+public class Human
+{
+String men="Men";
+String women="Women";
+public static void main(String[] args)
+{
+Human hb=new Human();
+hb.talk();
+}
+public void talk()
+{
+System.out.println(men);
+System.out.println(women);
+}
+}
