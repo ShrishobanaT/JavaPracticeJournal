6d3751d8f6adbe6eb64ab0ca0d4e39577330632f
ShrishobanaT <shrishobana_skystudent@outlook.com>
2022-03-25 10:09:53 +0100

    update

---
+++ IfblockBoolean.java
@@ -0,0 +1,15 @@
+public class IfblockBoolean
+{
+public static void main(String[] args)
+{
+boolean b=true;
+if(b=false)
+{
+System.out.println("Executes False");
+}
+else
+{
+System.out.println("Executes True");
+}
+}
+}
