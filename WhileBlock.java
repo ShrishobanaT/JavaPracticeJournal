03ff3c5f805e234eedb2e352a0e54b700cf5344e
ShrishobanaT <shrishobana_skystudent@outlook.com>
2022-03-25 14:04:01 +0100

    Update

---
+++ WhileBlock.java
@@ -0,0 +1,16 @@
+public class WhileBlock
+{
+public static void main(String[] args)
+{
+int a=10; 
+int b=20;
+while(a<b)
+{ 
+System.out.println("B is greater");
+}
+System.out.println("A is Smaller");
+}
+}
+// braces are optional and without braces 
+//we can take only one statement under while 
+//which should not be declarative statement.
