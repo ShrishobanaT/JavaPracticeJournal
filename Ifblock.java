9d70035f207beac156c69cd6553aa3c64ea35009
ShrishobanaT <shrishobana_skystudent@outlook.com>
2022-03-25 10:06:39 +0100

    Update

---
+++ Ifblock.java
@@ -0,0 +1,15 @@
+public class Ifblock
+{
+public static void main(String[] args)
+{
+int x=10;
+if(x==20)
+{
+System.out.println("Executes 20");
+}
+else
+{
+System.out.println("Executes 10");
+}
+}
+}
