25b228a258cd96ece8a4c033ecfeff3be8538a6a
ShrishobanaT <shrishobana_skystudent@outlook.com>
2022-03-25 14:08:39 +0100

    updated

---
+++ WhileBlock1.java
@@ -0,0 +1,12 @@
+public class WhileBlock1
+{
+public static void main(String[] args)
+{
+int a=10,b=20;
+while(a>b)
+{ 
+System.out.println("B is greater");
+}
+System.out.println("A is Smaller");
+}
+}
