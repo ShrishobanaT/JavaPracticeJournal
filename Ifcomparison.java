4573af3563c941c8e9504287f89baa01bc8bb33d
ShrishobanaT <shrishobana_skystudent@outlook.com>
2022-03-25 10:12:35 +0100

    Updated

---
+++ Ifcomparison.java
@@ -0,0 +1,15 @@
+public class Ifcomparison
+{
+public static void main(String[] args)
+{
+boolean b=true;
+if(b==true)
+{
+System.out.println("Executes True");
+}
+else
+{
+System.out.println("Executes False");
+}
+}
+}
