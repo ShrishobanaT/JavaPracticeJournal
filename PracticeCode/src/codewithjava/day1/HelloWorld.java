36de03694e0eea1401db5f372cd97f5fedc59d3e
ShrishobanaT <shrishobana_skystudent@outlook.com>
2022-03-31 22:58:12 +0200

    commit from Eclipse first program sample

---
+++ PracticeCode/src/codewithjava/day1/HelloWorld.java
@@ -0,0 +1,16 @@
+package codewithjava.day1;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to my First code in Eclipse");
+		HelloWorld HW= new HelloWorld();
+		HelloWorld.show();
+		
+		}
+	public static void show()
+	{
+		System.out.println("This is the method to display");
+	}
+}
