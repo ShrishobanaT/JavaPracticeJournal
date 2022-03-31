c94fdae98b506d59d5cda046ab0802d1cbe8b42c
ShrishobanaT <shrishobana_skystudent@outlook.com>
2022-03-31 23:31:37 +0200

    change sample

--- PracticeCode/src/codewithjava/day1/HelloWorld.java
+++ PracticeCode/src/codewithjava/day1/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Welcome to my First code in Eclipse");
+		System.out.println("Welcome to my First code Eclipse");
 		HelloWorld HW= new HelloWorld();
 		HelloWorld.show();
 		
